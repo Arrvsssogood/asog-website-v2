4ef1eeffefa7c71eddaa847aee9a9fa26acd7ae6
jpyxs <johnpsalcedo@my.cspc.edu.ph>
2026-07-03 12:17:56 +0800

    Delete unused loader files and SVG pictures
    SVG pictures to be replaced with PNG to optimize performance of the animation on low-end devices and slow connections

--- public/assets/loader/shaders/glow.js
+++
@@ -1,9 +0,0 @@
-export function createGlowMaterial(THREE, color, opacity = 0.2) {
-    return new THREE.MeshBasicMaterial({
-        color,
-        transparent: true,
-        opacity,
-        blending: THREE.AdditiveBlending,
-        depthWrite: false,
-    });
-}
